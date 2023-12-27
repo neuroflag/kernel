05a92742be774905e44a6da0ffa2a73ffa83db16
lzjie <lzjie@t-chip.com.cn>
2023-12-27 15:04:15 +0800

    drivers: media: modify the xvclk settings of gc2053 and gc2093

--- drivers/media/i2c/gc2093.c
+++ drivers/media/i2c/gc2093.c
@@ -147,6 +147,7 @@ struct gc2093 {
 	const struct gc2093_mode *cur_mode;
 
 	u32		module_index;
+	u32		clkout_enabled_index;
 	const char      *module_facing;
 	const char      *module_name;
 	const char      *len_name;
@@ -881,17 +882,19 @@ static int __gc2093_power_on(struct gc2093 *gc2093)
 	int ret;
 	struct device *dev = gc2093->dev;
 
-	ret = clk_set_rate(gc2093->xvclk, GC2093_XVCLK_FREQ);
-	if (ret < 0)
-		dev_warn(dev, "Failed to set xvclk rate\n");
+	if (gc2093->clkout_enabled_index){
+		ret = clk_set_rate(gc2093->xvclk, GC2093_XVCLK_FREQ);
+		if (ret < 0)
+			dev_warn(dev, "Failed to set xvclk rate\n");
 
-	if (clk_get_rate(gc2093->xvclk) != GC2093_XVCLK_FREQ)
-		dev_warn(dev, "xvclk mismatched, modes are based on 27MHz\n");
+		if (clk_get_rate(gc2093->xvclk) != GC2093_XVCLK_FREQ)
+			dev_warn(dev, "xvclk mismatched, modes are based on 27MHz\n");
 
-	ret = clk_prepare_enable(gc2093->xvclk);
-	if (ret < 0) {
-		dev_err(dev, "Failed to enable xvclk\n");
-		return ret;
+		ret = clk_prepare_enable(gc2093->xvclk);
+		if (ret < 0) {
+			dev_err(dev, "Failed to enable xvclk\n");
+			return ret;
+		}
 	}
 
 	if (gc2093->is_thunderboot)
@@ -918,13 +921,16 @@ static int __gc2093_power_on(struct gc2093 *gc2093)
 	return 0;
 
 disable_clk:
-	clk_disable_unprepare(gc2093->xvclk);
+	if (gc2093->clkout_enabled_index)
+		clk_disable_unprepare(gc2093->xvclk);
 	return ret;
 }
 
 static void __gc2093_power_off(struct gc2093 *gc2093)
 {
-	clk_disable_unprepare(gc2093->xvclk);
+	
+	if (gc2093->clkout_enabled_index)
+		clk_disable_unprepare(gc2093->xvclk);
 	if (gc2093->is_thunderboot) {
 		if (gc2093->is_first_streamoff) {
 			gc2093->is_thunderboot = false;
@@ -1779,13 +1785,22 @@ static int gc2093_probe(struct i2c_client *client,
 		return -EINVAL;
 	}
 
-	gc2093->is_thunderboot = IS_ENABLED(CONFIG_VIDEO_ROCKCHIP_THUNDER_BOOT_ISP);
+	ret = of_property_read_u32(node, "firefly,clkout-enabled-index", &gc2093->clkout_enabled_index);
 
-	gc2093->xvclk = devm_clk_get(gc2093->dev, "xvclk");
-	if (IS_ERR(gc2093->xvclk)) {
-		dev_err(gc2093->dev, "Failed to get xvclk\n");
-		return -EINVAL;
+	if (ret){
+                dev_err(dev, "could not get firefly,clkout-enabled-index, default output xvclk .");
+                gc2093->clkout_enabled_index = 1;
+        }
+
+	if (gc2093->clkout_enabled_index){
+		gc2093->xvclk = devm_clk_get(gc2093->dev, "xvclk");
+		if (IS_ERR(gc2093->xvclk)) {
+			dev_err(gc2093->dev, "Failed to get xvclk\n");
+			return -EINVAL;
+		}
 	}
+
+	gc2093->is_thunderboot = IS_ENABLED(CONFIG_VIDEO_ROCKCHIP_THUNDER_BOOT_ISP);
 
 	find_terminal_resolution(gc2093);
 
